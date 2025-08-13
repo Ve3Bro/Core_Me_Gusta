3a3ffe20e2fb202f32cf1bf55dcb6998fe633f03
Ve3Bro <vbrow03@gmail.com>
2025-08-12 21:16:36 -0400

    1st

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let boton1 = document.querySelector("#likesb1")
+let likes1 = document.querySelector("#likes1")
+var likes11 = 0
+
+boton1.addEventListener("click", function(){
+    likes11++
+    likes1.textContent = likes11
+})
+let boton2 = document.querySelector("#likesb2")
+let likes2 = document.querySelector("#likes2")
+var likes22 = 0
+
+boton2.addEventListener("click", function(){
+    likes22++
+    likes2.textContent = likes22
+})
+
+
+let boton3 = document.querySelector("#likesb3")
+let likes3 = document.querySelector("#likes3")
+var likes33 = 0
+
+boton3.addEventListener("click", function(){
+    likes33++
+    likes3.textContent = likes33
+})
